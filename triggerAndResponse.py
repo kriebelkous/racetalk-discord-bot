c917a6f6274ede52f8c7809c5a4a5a8588983624
Martin <mail@martinweterings.nl>
2025-04-14 19:09:12 +0000

    small change to test docker deployment

--- triggerAndResponse.py
+++ triggerAndResponse.py
@@ -2,6 +2,8 @@ def check_triggers(content):
     words = content.split()
     if "banaan" in words:
         return "Nog steeds bevroren?"
+    if "auto" in words:
+        return "Auto doet vroem vroem"
     if "cola" in words:
         return "voor de kleur"
     if len(words) == 1 and words[0] == "tijden":
