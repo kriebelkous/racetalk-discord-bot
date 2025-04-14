533909522486d8b37e733100cb3199bd83d9b675
Martin <mail@martinweterings.nl>
2025-04-14 19:14:32 +0000

    another small change

--- triggerAndResponse.py
+++ triggerAndResponse.py
@@ -6,8 +6,8 @@ def check_triggers(content):
         return "Auto doet vroem vroem"
     if "cola" in words:
         return "voor de kleur"
-    if len(words) == 1 and words[0] == "tijden":
-        return "hier zijn de tijden"
+    if len(words) == 1 and words[0] == "kutbot":
+        return "https://www.gifcen.com/wp-content/uploads/2022/03/middle-finger-gif-16.gif"
     if "infrastructuur" in words and len(words) > 1:
         return "mooi in een zin verwerkt zeg"
     return None
